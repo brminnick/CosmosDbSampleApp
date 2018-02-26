ad642086b0775c407e2500337696c4b671ed2b9d
Brandon Minnick <brandon.r.minnick@gmail.com>
2018-02-25 18:22:31 -0800

    Added UITest Project

--- CosmosDbSampleApp.iOS/AppDelegate.cs
+++ CosmosDbSampleApp.iOS/AppDelegate.cs
@@ -1,19 +1,26 @@
-﻿using UIKit;
+﻿using System.Diagnostics;
+
+using UIKit;
 using Foundation;
 
 namespace CosmosDbSampleApp.iOS
 {
-    [Register("AppDelegate")]
+    [Register(nameof(AppDelegate))]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
-        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        public override bool FinishedLaunching(UIApplication uiApplication, NSDictionary launchOptions)
         {
             global::Xamarin.Forms.Forms.Init();
             EntryCustomReturn.Forms.Plugin.iOS.CustomReturnEntryRenderer.Init();
 
+            ExposeAutomationAPIs();
+
             LoadApplication(new App());
 
-            return base.FinishedLaunching(app, options);
+            return base.FinishedLaunching(uiApplication, launchOptions);
         }
+
+        [Conditional("DEBUG")]
+        void ExposeAutomationAPIs() => Xamarin.Calabash.Start();
     }
 }
